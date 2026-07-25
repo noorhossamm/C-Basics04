dc262c6247cb482f60111a2b4d5e9b557554b68f
noorhossamm <noormwafy78@gmail.com>
2026-07-26 00:22:52 +0300

    Question02

--- Assignment02/Program.cs
+++ Assignment02/Program.cs
@@ -9,6 +9,16 @@
             //double[] prices = {25.5 , 40.0 , 33.75};
             //Console.WriteLine(prices[1]);
             #endregion
+
+            #region Question02
+            ////2--------------------
+            //int[,] shelfCopies =
+            //{
+            //    {3,5} ,
+            //    {1,4}  
+            //};
+            //Console.WriteLine(shelfCopies[1, 0]);
+            #endregion
         }
     }
 }
